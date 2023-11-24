49bcbbdfaeb1c737dc7ab802df5eb3d480b33903
Mohamed Kadi <handskadi@gmail.com>
2023-11-24 21:30:11 +0100

    Radical update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# Using Puppet, install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install flask
+exec { 'flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
 }
