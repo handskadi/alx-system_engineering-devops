6b702e4feef2d54e3a17938fe9b70418252a85a7
Mohamed KADI <handskadi@gmail.com>
2023-11-25 09:43:34 +0000

    Werb and Flast version sync

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install flask
-package { 'flask':
-  ensure   => '2.1.0',
+# Install Flask version 2.1.0
+package { 'Werkzeug':
+  ensure   => '2.0.0',
+  provider => 'pip3',
+}
+
+# Install Werkzeug version 2.0.0
+package { 'werkzeug':
+  ensure   => '2.0.0',
   provider => 'pip3',
 }
