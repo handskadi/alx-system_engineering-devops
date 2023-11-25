1e67542d2825315b89c202fc3bad0300acd36f33
Mohamed KADI <handskadi@gmail.com>
2023-11-25 09:47:56 +0000

    Version 2.1.0 sysnc in Flask and Wergzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # Install Flask version 2.1.0
-package { 'Werkzeug':
-  ensure   => '2.0.0',
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
 
 # Install Werkzeug version 2.0.0
 package { 'werkzeug':
-  ensure   => '2.0.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
