00c1e19ec8b22b84104e41638ea15f669b8df3c0
Moahmed KADI <handskadi@gmail.com>
2024-01-16 11:25:10 +0000

    Task fix 101

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """
-0-main
+this doc for module
 """
-import sys
+import requests
 
-if __name__ == "__main__":
-    number_of_subscribers = __import__("0-subs").number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+
+def number_of_subscribers(subreddit):
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return 0
