19c1f2a6b36931f40cd22e855fdd470f2e5242d8
Moahmed KADI <handskadi@gmail.com>
2024-01-16 11:19:36 +0000

    Fix 100

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,12 @@
-import requests
+#!/usr/bin/python3
+"""
+0-main
+"""
+import sys
 
-def number_of_subscribers(subreddit):
-    """
-    Retrieves the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: Number of subscribers if the subreddit is valid, otherwise 0.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'my-app/0.0.1'}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
-        return 0
-
-# Example usage
 if __name__ == "__main__":
-    import sys
-
+    number_of_subscribers = __import__("0-subs").number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        subreddit_name = sys.argv[1]
-        subscribers_count = number_of_subscribers(subreddit_name)
-        print(subscribers_count)
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
