f8d2fa573b91637c26b534956a7ff0cc41047135
Moahmed KADI <handskadi@gmail.com>
2024-01-16 10:50:44 +0000

    Task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-"""
-MK: number of subcs for a given sub reddit
-"""
-
-from requests import get
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns number of subs
-    (not active users, total subs) for a given subreddit.
-    """
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    if subreddit is None or not isinstance(subreddit, str):
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-
-    if response.status_code == 200:
-        try:
-            results = response.json()
-
-            # Check if 'data' key is present in the response
-            if 'data' in results:
-                # Check if 'subscribers' key is present in the 'data' dictionary
-                subscribers = results['data'].get('subscribers')
-                if subscribers is not None:
-                    return subscribers
-
-            # If 'data' or 'subscribers' key is missing, print an error message
-            print("Error: Unexpected response format")
-            return 0
-
-        except Exception as e:
-            print("Error parsing JSON: {}".format(e))
-            return 0
-    else:
-        print("Request failed with status code {}".format(response.status_code))
-        return 0
+    return sub_info.json().get("data").get("subscribers")
